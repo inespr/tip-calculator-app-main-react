3d59554609da41fd832cf93228dbf9f78a3ed4ae
inespr <iperrom@g.educaand.es>
2023-01-24 20:37:34 +0100

    actualización responsive

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './AppStyle.scss';
 
       return (
         <div className='App'>
+          <div className='h1'>
           <h1>SPLI</h1>
           <h1>TTER</h1>
+          </div>
           <div className='container'>
             <div className='column'>
                 <div className='row'>
@@ -14,20 +16,22 @@ import './AppStyle.scss';
                 </div>
                 <div className='row'>
                   <p>Select Tip %</p>
-                  <div className='columnbutton'>
-                    <input type={'button'} value="5%" id="5" name="tip" /*onClick={}*/></input>
-                    <input type={'button'} value="10%" id="10"name="tip" /*onClick={}*/></input>
-                    <input type={'button'} value="15%" id="15" name="tip" /*onClick={}*/></input>
-                  </div>
-                  <div className='columnbutton'>
-                    <input type={'button'} value="25%" id="25" name="tip" /*onClick={}*/></input>
-                    <input type={'button'} value="50%" id="50" name="tip" /*onClick={}*/></input>
-                    <input type={'number'} class="custom" placeholder="Custom" id="tipCustom" name="tipCust" /*onChange={}*/ pattern="^[0-9]+"></input>
+                  <div className='buttons'>
+                    <div className='columnbutton'>
+                      <input  className='button1' type={'button'} value="5%" id="5" name="tip" /*onClick={}*/></input>
+                      <input  className='button2' type={'button'} value="10%" id="10"name="tip" /*onClick={}*/></input>
+                      <input className='button3' type={'button'} value="15%" id="15" name="tip" /*onClick={}*/></input>
+                    </div>
+                    <div className='columnbutton'>
+                      <input  className='button4' type={'button'} value="25%" id="25" name="tip" /*onClick={}*/></input>
+                      <input  className='button5' type={'button'} value="50%" id="50" name="tip" /*onClick={}*/></input>
+                      <input  type={'number'} className="custom" placeholder="Custom" id="tipCustom" name="tipCust" /*onChange={}*/ pattern="^[0-9]+"></input>
+                    </div>
                   </div>
                 </div>
                 <div className='row'>
                   <p>Number of People</p>
-                  <p class="error" id="error">Can't be zero or decimal</p>
+                  <p className="error" id="error">Can't be zero or decimal</p>
                   <input type={'number'} placeholder="0" id="numP" min="0" pattern="^[0-9]+" /*onChange={}*//>
                 </div>
           </div>
@@ -36,13 +40,13 @@ import './AppStyle.scss';
               <div className='column-result'>
                 <div className='row-result1'>
                   <p >Tip Amount</p>
-                  <p class="per-person">/ person</p>
+                  <p className="per-person">/ person</p>
                   <p>Total</p>
-                <p class="per-person">/ person</p>
+                <p className="per-person">/ person</p>
                 </div>
                 <div className='row-result2'>
-                  <div class="result" id="resultTipAmount">$0.00</div>
-                  <div class="result" id="resultTotal">$0.00</div>
+                  <div className="result" id="resultTipAmount">$0.00</div>
+                  <div className="result" id="resultTotal">$0.00</div>
                 </div>
               </div>
               <div className='column-result'>
